fdef8090ca30f81d454d80f197fef68bf17df0cc
Alfred Bergström <beal23qm@student.ju.se>
2024-04-16 00:24:13 +0200

    local storage & trim task element
    fix local storages
    "save data" function
    fix "showtask" function
    
    note: had to trim taskElement, so x-mark from u00d7 dont duplicate after reload the site.

--- script.js
+++ script.js
@@ -14,17 +14,53 @@ function addTask() {
     li.appendChild(span);
 
     listContainer.appendChild(li);
+    saveData();
   }
   inputBox.value = "";
 }
+
 listContainer.addEventListener(
   "click",
-  function (a) {
-    if (a.target.tagName === "LI") {
-      a.target.classList.toggle("checked");
-    } else if (a.target.tagName === "SPAN") {
-      a.target.parentElement.remove();
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
     }
   },
   false
 );
+
+function saveData() {
+  const tasks = [];
+  const taskElements = document.querySelectorAll("#list-container li");
+  taskElements.forEach(function (taskElement) {
+    tasks.push(taskElement.childNodes[0].nodeValue.trim());
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function showTasks() {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  // Clear the listContainer
+  listContainer.innerHTML = "";
+
+  tasks.forEach(function (task) {
+    let li = document.createElement("li");
+    let taskText = document.createTextNode(task);
+    li.appendChild(taskText);
+
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+
+    listContainer.appendChild(li);
+  });
+}
+
+showTasks();
+
+window.addEventListener("load", showTasks);
